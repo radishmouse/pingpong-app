55010440ab9885e1ab48fb7273afb037560ce1d4
Chris Aquino <chris.aquino@gmail.com>
2018-11-01 10:14:00 -0400

    adds create table statements and inserts

--- schema.sql
+++ schema.sql
@@ -1,1 +1,20 @@
 -- should only contain the create table statements
+
+create table players (
+    id serial primary key,
+    name varchar(100),
+    -- games_won integer
+);
+
+create table games (
+    id serial primary key,
+
+    player1_score integer,
+    player2_score integer,
+
+    winner_id integer references players (id),
+
+    -- These are "foreign keys"
+    player1_id integer references players (id),
+    player2_id integer references players (id)
+);
--- seed.sql
+++ seed.sql
@@ -1,1 +1,21 @@
 -- fill the tables with dummy data
+
+insert into players
+    (name)
+values
+    ('jeff'),
+    ('chris'),
+    ('lorenzo'),
+    ('broseph'),
+    ('chravis'),
+    ('randy');
+
+insert into games
+    (player1_score, player2_score, player1_id, player2_id)
+values
+    (21, 0, 2, 3),
+    (21, 2, 2, 3),
+    (21, 1, 2, 3),
+    (13, 21, 3, 1),
+    (21, 2, 5, 1),
+    (10, 21, 4, 2);
